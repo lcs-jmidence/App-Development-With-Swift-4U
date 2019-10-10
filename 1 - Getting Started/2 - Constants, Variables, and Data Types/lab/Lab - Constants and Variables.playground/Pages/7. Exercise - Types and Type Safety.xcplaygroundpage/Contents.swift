6f69b0aaebcf15e07e53385336503556e18a0afe
Julio Midence <jmidence@lcs.on.ca>
2019-10-09 20:02:33 -0400

    Working on data types and realizing the difference between a constant and a variable

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,19 +3,19 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
  */
-let firstDecimal = 1.0
-let secondDecimal = 2.0
+var firstDecimal = 1.0
+var secondDecimal = 2.0
 
 /*:
  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
-
-
+var trueOrFalse = false
+print("Boolean types can't be assigned to double values")
 /*:
  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
 
-
+print("Can't convert value of type Double to specified type String")
 /*:
  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
  */
